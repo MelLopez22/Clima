be6b0a90626ead345272b5194f7ba0a904b0d928
Melissa Lopez <MelPruebas>
2022-08-05 04:01:14 -0300

    trabajando con el menu principal

---
+++ src/components/Clima.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Buscador from './Buscador'
+import MenuPrincipal from './MenuPrincipal'
+
+function Clima() {
+  return (
+    <div>
+
+      <MenuPrincipal />
+      <Buscador />
+    </div>
+  )
+}
+
+export default Clima
---
+++ src/components/MenuPrincipal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MenuPrincipal() {
+  return (
+    <div>HOLA SOY EL MENU PRINCIPAL</div>
+  )
+}
+
+export default MenuPrincipal
